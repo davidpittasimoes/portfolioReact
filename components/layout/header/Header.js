69fa268172a13bbf8ef8e26c9a257ed6697bb707
davidpittasimoes <davidpittasimoes@gmail.com>
2020-07-08 00:25:03 +0100

    fixed mobile layout

--- components/layout/header/Header.js
+++ components/layout/header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './styles.scss';
 import Grid from '@material-ui/core/Grid';
+import Link from 'next/link'; 
 
 const Header = (props) => {
     return(
         <div className="headerContainer">         
-            <h1 className="logoTitle">David Pitta-Simões</h1>
+            <Link href="/"><a><h1 className="logoTitle">David Pitta-Simões</h1></a></Link>
             <a href="./DDC_CARLOS_DAVID_DE_PITTA_SIMÕES_BRILHA_NUNES_CARLOSNATAGAP2DOTPT.pdf" download className="navItem btn secondary">Resume</a>
         </div>
      
--- components/screens/about/About.js
+++ components/screens/about/About.js
@@ -9,7 +9,8 @@ const Home = () => {
                 <div className="centeredPaper">
                     <div className="pageContent about">
                             <h2 className="screenSectionHeader"><span>02.</span> About me</h2>
-                            <p>I’m David, an UI / UX designer and Front-end developer based in Brussels. I’m kinda hybrid when it comes to what I do. I’ve been doing both development and UI design for the last 5 years and I a have love for both. So if you are wondering which one I prefer or I can do bets... I would both, for different reasons in diferent times and envoirnments.</p>
+                            <p>I’m David, an UI / UX designer and Front-end developer based in Brussels. I see myself as an hybrid when it comes to what I do. I’ve been doing both development and UI design for the last 5 years and I a have love for both. I love the psichology behind UX, the creativity in UI and the constant challenge of development.</p>
+                            <p>My work is not my only passion, I also love sports, sun, surf, books and pretty much anything that makes me laugh!</p>
                     </div>
                     <div className="floatingItemContainer">
                             <figure>
